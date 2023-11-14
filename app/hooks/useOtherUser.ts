0dd2a6f9656c405ff44588f70f231dd2ea5d589b
trungtan <trungtan14@gmail.com>
2023-11-14 16:28:52 +0700

    add endcoding

--- app/hooks/useOtherUser.ts
+++ app/hooks/useOtherUser.ts
@@ -1,28 +1,20 @@
-import { User } from "@prisma/client";
-import { FullConversationType } from "../types";
 import { useSession } from "next-auth/react";
 import { useMemo } from "react";
+import { FullConversationType } from "../types";
+import { User } from "@prisma/client";
 
-const userOtherUser = (
-  conversation:
-    | FullConversationType
-    | {
-        users: User[];
-      }
-) => {
+const useOtherUser = (conversation: FullConversationType | { users: User[] }) => {
   const session = useSession();
 
   const otherUser = useMemo(() => {
-    const currentUserEmail = session?.data?.user?.email;
+    const currentUserEmail = session.data?.user?.email;
 
-    const otherUser = conversation.users.filter(
-      (user) => user.email !== currentUserEmail
-    );
+    const otherUser = conversation.users.filter((user) => user.email !== currentUserEmail);
 
     return otherUser[0];
-  }, [session?.data?.user?.email, conversation.users]);
+  }, [session.data?.user?.email, conversation.users]);
 
   return otherUser;
 };
 
-export default userOtherUser
+export default useOtherUser;
